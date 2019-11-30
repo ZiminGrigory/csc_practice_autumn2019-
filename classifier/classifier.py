064fe58f8174f9b3975764d1444460638a0a0061
Grigorii Zimin <gzimin@luxoft.com>
2019-11-30 12:47:43 +0300

    initial:
    1) raw readme
    2) dummy-classifier

---
+++ classifier/classifier.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import os
+import csv
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import pandas as pd
+from functools import reduce
+import pickle
+
+def distance(p1x,p1y,p2x,p2y):
+	return (p2x-p1x)**2 + (p2y-p1y)**2
+
+def getClassifier(pathToPkl):
+	with open(pathToPkl, 'rb') as fid:
+		classifier = pickle.load(fid)
+		return classifier
+
+def predict(classifier, data):
+	distanceData = []
+	distanceData.append(distance(data[2],data[3],data[10],data[11]))
+	distanceData.append(distance(data[4],data[5],data[8],data[9]))
+	distanceData.append(distance(data[2+12],data[3+12],data[10+12],data[11+12]))
+	distanceData.append(distance(data[4+12],data[5+12],data[8+12],data[9+12]))
+	return classifier.predict([distanceData])[0]
+
+def createPredictor(classifier):
+	def predict(data):
+		distanceData = []
+		distanceData.append(distance(data[2],data[3],data[10],data[11]))
+		distanceData.append(distance(data[4],data[5],data[8],data[9]))
+		distanceData.append(distance(data[2+12],data[3+12],data[10+12],data[11+12]))
+		distanceData.append(distance(data[4+12],data[5+12],data[8+12],data[9+12]))
+		return classifier.predict([distanceData])[0]
+
+	return predict
