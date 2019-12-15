10676f831c09f4ec49830e330da0e35f6a872713
Grigorii Zimin <gzimin@luxoft.com>
2019-12-15 17:17:17 +0300

    more data for classifier, new version of it

--- MainCycle.py
+++ MainCycle.py
@@ -3,8 +3,8 @@ sys.path.append('dan_tf/DAN_V2')
 sys.path.append('classifier')  
 sys.path.append('keras-yolo3') 
 
-from  My_DAN_V2 import storeFramesWithMarks, storeEyesData, storeFramesWithMarksAndPredictRes
-from my_web_predict import  createCameraGenerator, createCameraGeneratorWithCount
+from  My_DAN_V2 import storeFramesWithMarks, storeEyesData, storeEyesDataAndImages, showFramesWithMarksAndPredictRes
+from my_web_predict import  createCameraGenerator, createCameraGeneratorWithCount, folderGenerator, createVideoGenerator
 from classifier import  getClassifier, createPredictor
 
 
@@ -18,7 +18,18 @@ from classifier import  getClassifier, createPredictor
 # storeEyesData(gen, "/home/greg/dev/csc_practice_autumn2019/300W_HELEN/eyesClose.csv")
 
 
-# stored my face on disk with landmarks
-gen = createCameraGenerator(112,68);
-clsPredict = createPredictor(getClassifier('classifier/my_dumped_classifier.pkl'))
-storeFramesWithMarksAndPredictRes(gen, clsPredict)
+# camera video result -- uncomment
+# gen = createCameraGenerator(112,68);
+# clsPredict = createPredictor(getClassifier('classifier/my_dumped_classifier2.pkl'))
+# showFramesWithMarksAndPredictRes(gen, clsPredict)
+
+#video
+gen = createVideoGenerator(112,68, '/home/greg/Videos/salma.mp4');
+clsPredict = createPredictor(getClassifier('classifier/my_dumped_classifier2.pkl'))
+showFramesWithMarksAndPredictRes(gen, clsPredict)
+
+
+
+
+# gen = folderGenerator(112, 68, '/home/greg/dev/csc_practice_autumn2019/facesForClassifier/images/2/')
+# storeEyesDataAndImages(gen, '/home/greg/dev/csc_practice_autumn2019/facesForClassifier/images/2_res/')
--- classifier/classifier.py
+++ classifier/classifier.py
@@ -10,28 +10,52 @@ from functools import reduce
 import pickle
 
 def distance(p1x,p1y,p2x,p2y):
-	return (p2x-p1x)**2 + (p2y-p1y)**2
+    return (p2x-p1x)**2 + (p2y-p1y)**2
 
 def getClassifier(pathToPkl):
-	with open(pathToPkl, 'rb') as fid:
-		classifier = pickle.load(fid)
-		return classifier
+    with open(pathToPkl, 'rb') as fid:
+        classifier = pickle.load(fid)
+        return classifier
+
+# def predict(classifier, data):
+# 	distanceData = []
+# 	distanceData.append(distance(data[2],data[3],data[10],data[11]))
+# 	distanceData.append(distance(data[4],data[5],data[8],data[9]))
+# 	distanceData.append(distance(data[2+12],data[3+12],data[10+12],data[11+12]))
+# 	distanceData.append(distance(data[4+12],data[5+12],data[8+12],data[9+12]))
+# 	return classifier.predict([distanceData])[0]
+
+# def createPredictor(classifier):
+# 	def predict(data):
+# 		distanceData = []
+# 		distanceData.append(distance(data[2],data[3],data[10],data[11]))
+# 		distanceData.append(distance(data[4],data[5],data[8],data[9]))
+# 		distanceData.append(distance(data[2+12],data[3+12],data[10+12],data[11+12]))
+# 		distanceData.append(distance(data[4+12],data[5+12],data[8+12],data[9+12]))
+# 		return classifier.predict([distanceData])[0]
+
+# 	return predict
 
 def predict(classifier, data):
-	distanceData = []
-	distanceData.append(distance(data[2],data[3],data[10],data[11]))
-	distanceData.append(distance(data[4],data[5],data[8],data[9]))
-	distanceData.append(distance(data[2+12],data[3+12],data[10+12],data[11+12]))
-	distanceData.append(distance(data[4+12],data[5+12],data[8+12],data[9+12]))
-	return classifier.predict([distanceData])[0]
+    distanceData = [
+        distance(data[2],data[3],data[10],data[11]),
+        distance(data[4],data[5],data[8],data[9]),
+        distance(data[0],data[1],data[6],data[7]),
+        distance(data[2+12],data[3+12],data[10+12],data[11+12]),
+        distance(data[4+12],data[5+12],data[8+12],data[9+12]),
+        distance(data[0+12],data[1+12],data[6+12],data[7+12])]
+    return classifier.predict([distanceData])[0]
 
 def createPredictor(classifier):
-	def predict(data):
-		distanceData = []
-		distanceData.append(distance(data[2],data[3],data[10],data[11]))
-		distanceData.append(distance(data[4],data[5],data[8],data[9]))
-		distanceData.append(distance(data[2+12],data[3+12],data[10+12],data[11+12]))
-		distanceData.append(distance(data[4+12],data[5+12],data[8+12],data[9+12]))
-		return classifier.predict([distanceData])[0]
-
-	return predict
+    def predict(data):
+        distanceData = [
+            distance(data[2],data[3],data[10],data[11]),
+            distance(data[4],data[5],data[8],data[9]),
+            distance(data[0],data[1],data[6],data[7]),
+            distance(data[2+12],data[3+12],data[10+12],data[11+12]),
+            distance(data[4+12],data[5+12],data[8+12],data[9+12]),
+            distance(data[0+12],data[1+12],data[6+12],data[7+12])]
+        return classifier.predict([distanceData])[0]
+    
+    
+    return predict
